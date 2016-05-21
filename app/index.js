9433596632d4befb01de1c2c1a8cfb395e3a249c
David Weldon <dweldon@gmail.com>
2016-05-21 13:26:54 -0700

    video: 6

--- app/index.js
+++ app/index.js
@@ -1,5 +1,22 @@
 import { createStore } from 'redux';
 
-console.log(createStore);
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1;
+    case 'DECREMENT': return state - 1;
+    default: return state;
+  }
+};
 
-document.body.innerText = 'hello world!';
+const store = createStore(counter);
+
+const render = () => {
+  document.body.innerText = store.getState();
+};
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+});
