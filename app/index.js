e2ae76351fa58ed1dc20c0167d1e4504fe7c1863
David Weldon <dweldon@gmail.com>
2016-05-21 13:18:37 -0700

    Add initial files.

---
+++ app/index.js
@@ -0,0 +1,5 @@
+import { createStore } from 'redux';
+
+console.log(createStore);
+
+document.body.innerText = 'hello world!';
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+process.env.BABEL_ENV = TARGET;
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+};
+
+const config = {
+  entry: {
+    app: PATHS.app,
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    root: PATHS.app,
+    extensions: ['', '.js', '.jsx'],
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        include: PATHS.app,
+      },
+    ],
+  },
+};
+
+if (TARGET === 'start' || !TARGET) {
+  Object.assign(config, {
+    devtool: 'eval',
+    devServer: {
+      contentBase: PATHS.build,
+      progress: true,
+      stats: 'errors-only',
+    },
+  });
+}
+
+module.exports = config;
