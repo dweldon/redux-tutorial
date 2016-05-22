d473c8d572436e7c91bd5f1b71c316bf6bbb4a7a
David Weldon <dweldon@gmail.com>
2016-05-21 20:50:06 -0700

    video: 9

--- app/index.jsx
+++ app/index.jsx
@@ -30,3 +30,58 @@ const render = () => {
 
 store.subscribe(render);
 render();
+
+//------------------------------------------------------------------------------
+
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+const addCounter = (list, value) => [...list, value];
+
+const testAddCounter = () => {
+  const l1 = [];
+  const l2 = [0];
+
+  deepFreeze(l1);
+
+  expect(addCounter(l1, 0)).toEqual(l2);
+};
+
+testAddCounter();
+
+//------------------------------------------------------------------------------
+
+const removeCounter = (list, index) => [
+  ...list.slice(0, index),
+  ...list.slice(index + 1),
+];
+
+const testRemoveCounter = () => {
+  const l1 = [0, 10, 20];
+  const l2 = [0, 20];
+
+  deepFreeze(l1);
+
+  expect(removeCounter(l1, 1)).toEqual(l2);
+};
+
+testRemoveCounter();
+
+//------------------------------------------------------------------------------
+
+const incrementCounter = (list, index) => [
+  ...list.slice(0, index),
+  list[index] + 1,
+  ...list.slice(index + 1),
+];
+
+const testIncrementCounter = () => {
+  const l1 = [0, 10, 20];
+  const l2 = [0, 11, 20];
+
+  deepFreeze(l1);
+
+  expect(incrementCounter(l1, 1)).toEqual(l2);
+};
+
+testIncrementCounter();
