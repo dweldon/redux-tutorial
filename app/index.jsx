dac3b1c62078329a3f9cb2ba9a73008139ea80ba
David Weldon <dweldon@gmail.com>
2016-05-21 22:44:53 -0700

    video: 10

--- app/index.jsx
+++ app/index.jsx
@@ -36,52 +36,23 @@ render();
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
-const addCounter = (list, value) => [...list, value];
+const toggleTodo = (todo) => ({ ...todo, completed: !todo.completed });
 
-const testAddCounter = () => {
-  const l1 = [];
-  const l2 = [0];
+const testToggleTodo = () => {
+  const t1 = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false,
+  };
+  const t2 = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true,
+  };
 
-  deepFreeze(l1);
+  deepFreeze(t1);
 
-  expect(addCounter(l1, 0)).toEqual(l2);
+  expect(toggleTodo(t1)).toEqual(t2);
 };
 
-testAddCounter();
-
-//------------------------------------------------------------------------------
-
-const removeCounter = (list, index) => [
-  ...list.slice(0, index),
-  ...list.slice(index + 1),
-];
-
-const testRemoveCounter = () => {
-  const l1 = [0, 10, 20];
-  const l2 = [0, 20];
-
-  deepFreeze(l1);
-
-  expect(removeCounter(l1, 1)).toEqual(l2);
-};
-
-testRemoveCounter();
-
-//------------------------------------------------------------------------------
-
-const incrementCounter = (list, index) => [
-  ...list.slice(0, index),
-  list[index] + 1,
-  ...list.slice(index + 1),
-];
-
-const testIncrementCounter = () => {
-  const l1 = [0, 10, 20];
-  const l2 = [0, 11, 20];
-
-  deepFreeze(l1);
-
-  expect(incrementCounter(l1, 1)).toEqual(l2);
-};
-
-testIncrementCounter();
+testToggleTodo();
