398ab6091a11a4b583aef755412d56a5b40492a8
David Weldon <dweldon@gmail.com>
2016-05-23 12:17:44 -0700

    video: 16

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,13 @@
-import { combineReducers } from 'redux';
+import _ from 'underscore';
 
 import todos from './todos';
 import visibilityFilter from './visibilityFilter';
+
+const combineReducers = (reducers) =>
+  (state = {}, action) =>
+    _.keys(reducers).reduce((
+      (nextState, key) =>
+        ({ ...nextState, [key]: reducers[key](state[key], action) })
+    ), {});
 
 export default combineReducers({ todos, visibilityFilter });
