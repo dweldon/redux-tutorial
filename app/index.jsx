251b4d8f7ba0dcc25467f4fcce139f438f3f7e98
David Weldon <dweldon@gmail.com>
2016-05-24 16:45:07 -0700

    video: 26

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
 
 import store from './store';
 import TodoApp from './components/TodoApp';
-import Provider from './components/Provider';
 
 ReactDom.render(
   <Provider store={store}>
