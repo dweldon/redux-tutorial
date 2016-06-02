09d39ece891125a1ea0dc462972303da2823bf4c
David Weldon <dweldon@gmail.com>
2016-06-02 14:52:42 -0700

    ir: 5

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import TodoApp from './TodoApp';
 
 /* eslint-disable react/prop-types */
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <TodoApp />
+    <Router history={browserHistory}>
+      <Route path="/" component={TodoApp} />
+    </Router>
   </Provider>
 );
 
