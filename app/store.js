b589f9f5b294e162b31f22668dc479bd5a82aad5
David Weldon <dweldon@gmail.com>
2016-06-03 11:12:28 -0700

    ir: 12

--- app/store.js
+++ app/store.js
@@ -3,12 +3,35 @@ import throttle from 'lodash/throttle';
 import rootReducer from './reducers/index';
 import { loadState, saveState } from './localStorage';
 
+/* eslint-disable no-console*/
+
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) return rawDispatch;
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
+/* eslint-enable no-console*/
+
 const configureStore = () => {
   const persistedState = loadState();
 
   const store = createStore(rootReducer, persistedState,
     window.devToolsExtension && window.devToolsExtension()
   );
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   store.subscribe(throttle(() => {
     saveState({
