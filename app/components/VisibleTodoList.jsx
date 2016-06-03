97661c85a6fc6037cfaff559d9477bdba7fd52cd
David Weldon <dweldon@gmail.com>
2016-06-03 12:09:33 -0700

    ir: 14

--- app/components/VisibleTodoList.jsx
+++ app/components/VisibleTodoList.jsx
@@ -1,16 +1,40 @@
+import React from 'react';
 import { connect } from 'react-redux';
-
 import TodoList from './TodoList';
 import { toggleTodo } from '../actions';
 import { getVisibleTodos } from '../reducers';
+import { fetchTodos } from '../api';
+
+/* eslint-disable react/prop-types */
+
+class VisibleTodoList extends React.Component {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos =>
+      console.log(this.props.filter, todos)
+    );
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos =>
+        console.log(this.props.filter, todos)
+      );
+    }
+  }
+
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
 
 const mapStateToProps = (state, ownProps) => ({
-  todos: getVisibleTodos(state, ownProps.filter),
+  filter: ownProps.filter,
+  todos: getVisibleTodos(state),
 });
 
-const VisibleTodoList = connect(
+const VisibleTodoListWrapper = connect(
   mapStateToProps,
   { onTodoClick: toggleTodo }
-)(TodoList);
+)(VisibleTodoList);
 
-export default VisibleTodoList;
+export default VisibleTodoListWrapper;
