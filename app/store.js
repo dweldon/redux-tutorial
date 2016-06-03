0dc5a73f2b064fd7fcbdc5123955e7b9285ffb95
David Weldon <dweldon@gmail.com>
2016-06-03 16:58:59 -0700

    ir: 17

--- app/store.js
+++ app/store.js
@@ -3,15 +3,14 @@ import rootReducer from './reducers/index';
 
 /* eslint-disable no-console*/
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  if (!console.group) return rawDispatch;
+const logger = (store) => (next) => {
+  if (!console.group) return next;
 
   return (action) => {
     console.group(action.type);
     console.log('%c prev state', 'color: gray', store.getState());
     console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log('%c next state', 'color: green', store.getState());
     console.groupEnd(action.type);
     return returnValue;
@@ -20,26 +19,39 @@ const addLoggingToDispatch = (store) => {
 
 /* eslint-enable no-console*/
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  };
+/* eslint-disable no-unused-vars */
+
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next);
+  }
+  return next(action);
 };
+
+/* eslint-enable no-unused-vars */
+
+/* eslint-disable no-param-reassign, no-return-assign */
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware =>
+    store.dispatch = middleware(store)(store.dispatch)
+  );
+};
+
+/* eslint-enable no-param-reassign, no-return-assign */
 
 const configureStore = () => {
   const store = createStore(rootReducer, {},
     window.devToolsExtension && window.devToolsExtension()
   );
 
+  const middlewares = [promise];
+
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store);
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store;
 };
