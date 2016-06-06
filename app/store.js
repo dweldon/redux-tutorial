61e8da9089f0276ecb78efce3ad81daf7a1b41cd
David Weldon <dweldon@gmail.com>
2016-06-06 10:46:28 -0700

    ir: 22

--- app/store.js
+++ app/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import promise from 'redux-promise';
 import createLogger from 'redux-logger';
 import rootReducer from './reducers/index';
 
+const thunk = (store) => (next) => (action) =>
+  typeof action === 'function' ? action(store.dispatch) : next(action);
+
 const configureStore = () => {
-  const middleware = [promise];
+  const middleware = [thunk];
   if (process.env.NODE_ENV !== 'production') {
     middleware.push(createLogger());
   }
